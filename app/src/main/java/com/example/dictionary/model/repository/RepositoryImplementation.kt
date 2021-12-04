d1462cf5e7929d5676aae782f44afa7e1ac2a415
Sergey <gecktor87@gmail.com>
2021-12-04 16:29:01 +0300

    Внедрил зависимости Koin, реализовано асинхронное выполнения задач при помощи Корутин

--- app/src/main/java/com/example/dictionary/model/repository/RepositoryImplementation.kt
+++ app/src/main/java/com/example/dictionary/model/repository/RepositoryImplementation.kt
@@ -2,12 +2,11 @@ package com.example.dictionary.model.repository
 
 import com.example.dictionary.model.data.DataModel
 import com.example.dictionary.model.datasource.DataSource
-import io.reactivex.Observable
 
 class RepositoryImplementation(private val dataSource: DataSource<List<DataModel>>) :
     Repository<List<DataModel>> {
 
-    override fun getData(word: String): Observable<List<DataModel>> {
+    override suspend fun getData(word: String): List<DataModel> {
         return dataSource.getData(word)
     }
 }
